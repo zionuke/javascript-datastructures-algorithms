b592d75e0187dbd262da074d5784dc60ee42c223
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-06 18:10:22 +0800

    封装队列结构

--- src/Queue/queue.js
+++ src/Queue/queue.js
@@ -1,41 +1,45 @@
-// 队列结构的封装
+// 封装队列类
 export default class Queue {
 
+  // 属性
   constructor() {
-    this.items = [];
+    this.items = []
   }
 
-  // enqueue(item) 入队，将元素加入到队列中
-  enqueue(item) {
-    this.items.push(item);
+  // 方法
+  // 1.将元素加入到队列中
+  enqueue(element) {
+    this.items.push(element)
   }
 
-  // dequeue() 出队，从队列中删除队头元素，返回删除的那个元素
+  // 2.删除队列前端元素
   dequeue() {
-    return this.items.shift();
+    return this.items.shift()
   }
 
-  // front() 查看队列的队头元素
+  // 3.查看队列前端的元素
   front() {
-    return this.items[0];
+    return this.items[0]
   }
 
-  // isEmpty() 查看队列是否为空
+  // 4.查看队列是否为空
   isEmpty() {
-    return this.items.length === 0;
+    return this.items.length === 0
   }
 
-  // size() 查看队列中元素的个数
+  // 5.查看队列中元素个数
   size() {
-    return this.items.length;
+    return this.items.length
   }
 
-  // toString() 将队列中的元素以字符串形式返回
+  // 6.toString方法
   toString() {
-    let result = '';
-    for (let item of this.items) {
-      result += item + ' ';
+    let resultString = ''
+    for (const item of this.items) {
+      resultString += item + ' '
     }
-    return result;
+    return resultString
   }
+
+  
 }
