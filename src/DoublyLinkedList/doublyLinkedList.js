7992ed51818aed03de3cd6c0cc6294857db6c485
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-07 21:14:24 +0800

    双向链表append方法实现

--- src/DoublyLinkedList/doublyLinkedList.js
+++ src/DoublyLinkedList/doublyLinkedList.js
@@ -17,4 +17,35 @@ export class DoublyLinkedList extends LinkedList {
     super()
     this.tail = null
   }
+
+  // ------------ 链表的常见操作 ------------ //
+
+  // append(element) 往双向链表尾部追加一个新的元素
+  // 重写 append()
+  append(data) {
+
+    // 1、创建新节点
+    const newNode = new DoublyNode(data)
+
+    // 2、追加新节点
+    if (this.length === 0) {
+      // 链表长度为 0 时，直接修改头指针head即可
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      // ！！跟单向链表不同,不用通过循环找到最后一个节点,因为有尾指针
+      // 当添加一个节点时，涉及3个指针要修改
+      // 1、原来的尾节点的next指针要指向新节点
+      // 2、新节点的prev指针要指向原来的尾节点
+      // 3、尾指针要指向新节点
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+
+    // 3、追加完新节点后，链表长度 + 1
+    this.length++
+  }
+
+  
 }
