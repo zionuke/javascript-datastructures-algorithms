527a2157715030781d8b1d6db076830d0f341195
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-08 12:19:02 +0800

    双向链表测试完毕

--- src/DoublyLinkedList/index.js
+++ src/DoublyLinkedList/index.js
@@ -5,44 +5,50 @@ console.log('// ----- 双向链表结构测试 START -----//');
 const doublyLinkedList = new DoublyLinkedList();
 
 // append() 测试
+console.log('append() 测试');
 doublyLinkedList.append('ZZ');
 doublyLinkedList.append('XX');
 doublyLinkedList.append('CC');
-console.log(doublyLinkedList);
+console.log(doublyLinkedList.toString()); //--> ZZ XX CC
 
 // insert() 测试
+console.log('insert() 测试');
 doublyLinkedList.insert(0, '00');
 doublyLinkedList.insert(2, '22');
-console.log(doublyLinkedList);
+console.log(doublyLinkedList.toString()); //--> 00 ZZ 22 XX CC
 
 // getData() 测试
+console.log('getData() 测试');
 console.log(doublyLinkedList.getData(1)); //--> ZZ
 
 // indexOf() 测试
+console.log('indexOf() 测试');
 console.log(doublyLinkedList.indexOf('XX')); //--> 3
-console.log(doublyLinkedList);
 
 // removeAt() 测试
-// doublyLinkedList.removeAt(0);
-// doublyLinkedList.removeAt(1);
-// console.log(doublyLinkedList);
+console.log('removeAt() 测试');
+doublyLinkedList.removeAt(0);
+doublyLinkedList.removeAt(1);
+console.log(doublyLinkedList.toString()); //--> ZZ XX CC
 
 // update() 测试
 console.log('update() 测试');
 doublyLinkedList.update(0, '111111');
-console.log(doublyLinkedList);
+console.log(doublyLinkedList.toString()); //--> 111111 XX CC
 
 // remove() 测试
 console.log('remove() 测试');
 console.log(doublyLinkedList.remove('111111'));
-// console.log(doublyLinkedList.remove('22222'));
-console.log(doublyLinkedList);
+// console.log(doublyLinkedList.remove('XX'));
+console.log(doublyLinkedList.toString()); //--> XX CC
 
 // forwardToString() 测试
-// console.log(doublyLinkedList.forwardToString());
+console.log('forwardToString() 测试');
+console.log(doublyLinkedList.forwardToString()); //--> XX--CC--
 
 // backwardString() 测试
-// console.log(doublyLinkedList.backwardString());
+console.log('backwardString() 测试');
+console.log(doublyLinkedList.backwardString()); //--> CC--XX--
 
 console.log('// ----- 双向链表结构测试 END -----//');
 
--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@
 // import './PriorityQueue'
 
 // 导入单向链表结构的封装及测试代码
-import './LinkedList'
+// import './LinkedList'
 
 // 导入双向链表结构的封装及测试代码
-// import './DoublyLinkedList'
+import './DoublyLinkedList'
 
 // 导入集合结构的封装及测试代码
 // import './Set'
