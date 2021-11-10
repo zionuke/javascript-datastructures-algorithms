213d76dfbbd2eccd445c0442d0640c94ab0055f3
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-10 10:26:50 +0800

    添加clear方法

--- src/Stack/stack-array.js
+++ src/Stack/stack-array.js
@@ -1,32 +1,43 @@
 //封装栈类
 export default class Stack {
-  // 栈中的属性
+  // 用数组来保存栈内元素
   constructor() {
     this.items = []
   }
 
   // 栈的相关操作
-  // 1.将元素压入栈
+
+  // 1.添加一个（或几个）新元素到栈顶
   push(element) {
     this.items.push(element)
   }
-  // 2.从栈中取出元素
+
+  // 2.移除栈顶的元素，同时返回被移除的元素
   pop() {
     return this.items.pop()
   }
-  // 3.查看一下栈顶元素
+
+  // 3.返回栈顶的元素，不对栈做任何修改
   peek() {
     return this.items[this.items.length-1]
   }
-  // 4.判断栈是否为空
+
+  // 4.判断栈是否为空，如果栈里没有任何元素就返回true，否则返回false
   isEmpty() {
     return this.items.length === 0
   }
+
   // 5.获取栈中元素个数
   size() {
     return this.items.length
   }
-  // 6.toString方法
+
+  // 6.移除栈里所有元素
+  clear() {
+    this.items = []
+  }
+
+  // 7.toString方法
   toString() {
     let resultString = ''
     for (const item of this.items) {
@@ -34,5 +45,4 @@ export default class Stack {
     }
     return resultString
   }
-
 }
