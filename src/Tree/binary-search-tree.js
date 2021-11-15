2a30579fdf29d94fb5fafab05db43664c562842b
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-15 14:31:24 +0800

    封装二叉搜索树

---
+++ src/Tree/binary-search-tree.js
@@ -0,0 +1,15 @@
+// 节点类
+class Node {
+  constructor() {
+    this.key = key
+    this.left = null
+    this.right = null
+  }
+}
+
+// 封装二叉搜索树（特点：左子树节点值 < 根节点，右子树节点值 > 根节点）
+export default class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+}
