793ed0ea4473d818ea167d18e9137e3ebd4337f2
dragon-liu <liuzhichao@hust.edu.cn>
2021-11-17 23:00:30 +0800

    实现添加顶点方法

--- src/Graph/graph.js
+++ src/Graph/graph.js
@@ -6,4 +6,17 @@ export default class Graph {
     this.vertices = [] // 用数组存储顶点的名字
     this.adjList = new Dictionary() //用字典来存储邻接表
   }
+
+  // 添加顶点
+  addVertex(v) {
+    // 顶点不存在于图中时才添加
+    if (!this.vertices.includes(v)) {
+      // 将顶点添加到顶点列表中
+      this.vertices.push(v)
+      // 在邻接表中，设置顶点v作为键，对应的字典值为一个空数组
+      this.adjList.set(v, [])
+    }
+  }
+
+
 }
